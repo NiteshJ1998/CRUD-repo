5245c4f8d255f76738f9260fcdd93610f1d68be0
NiteshJ1998 <niteshjaiswar10@gmail.com>
2023-09-15 23:33:58 +0530

    adding file to rep

---
+++ Components/ItemCRUD.js
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import UserData from "./UserData";
+
+function ItemCRUD() {
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState("");
+  const [editingItem, setEditingItem] = useState(null);
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Replace 'your-api-endpoint' with the actual API endpoint you want to fetch data from
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments")
+      .then((response) => {
+        // Use slice to get only the first 10 results
+        const first10Results = response.data.slice(0, 10);
+        setData(first10Results);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  //   useEffect(() => {
+  //     // Fetch items from your API or a local data source
+  //     // For this example, we'll use a hardcoded array
+  //     const initialItems = [
+  //       { id: 1, name: "Item 1 " },
+  //       { id: 2, name: "Item 2 " },
+  //       { id: 3, name: "Item 3 " },
+  //     ];
+  //     setItems(initialItems);
+  //   }, []);
+
+  const handleAddItem = () => {
+    // Create a new item and add it to the list
+    const newItemObj = { id: items.length + 1, name: newItem };
+    setItems([newItemObj, ...items]);
+    setNewItem("");
+  };
+
+  const handleEditItem = (id) => {
+    // Set the item you want to edit
+    const itemToEdit = items.find((item) => item.id === id);
+    setEditingItem(itemToEdit);
+  };
+
+  const handleUpdateItem = () => {
+    // Update the item
+    const updatedItems = items.map((item) =>
+      item.id === editingItem.id ? { ...item, name: editingItem.name } : item
+    );
+    setItems(updatedItems);
+    setEditingItem(null);
+  };
+
+  const handleDeleteItem = (id) => {
+    // Delete the item
+    const updatedItems = items.filter((item) => item.id !== id);
+    setItems(updatedItems);
+  };
+
+  return (
+    <div>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {editingItem?.id === item.id ? (
+              <input
+                type="text"
+                value={editingItem.name}
+                onChange={(e) =>
+                  setEditingItem({ ...editingItem, name: e.target.value })
+                }
+              />
+            ) : (
+              item.name
+            )}
+            <button onClick={() => handleEditItem(item.id)}>Edit</button>
+            <button onClick={() => handleDeleteItem(item.id)}>Delete</button>
+            {editingItem?.id === item.id && (
+              <button onClick={handleUpdateItem}>Update</button>
+            )}
+          </li>
+        ))}
+      </ul>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            <UserData data={data} />
+          </tbody>
+        </table>
+        <h1>CRUD Operations</h1>
+        <input
+          type="text"
+          placeholder="New item"
+          value={newItem}
+          onChange={(e) => data.push(e.target.value)}
+        />
+        <button onClick={handleAddItem}>Add</button>
+      </div>
+    </div>
+  );
+}
+
+export default ItemCRUD;
---
+++ Components/UserData.js
@@ -0,0 +1,18 @@
+const UserData = ({ data }) => {
+  return (
+    <>
+      {data.map((curUser) => {
+        const { id, name, email } = curUser;
+
+        return (
+          <tr key={id}>
+            <td>{id}</td>
+            <td>{name}</td>
+            <td>{email}</td>
+          </tr>
+        );
+      })}
+    </>
+  );
+};
+export default UserData;
